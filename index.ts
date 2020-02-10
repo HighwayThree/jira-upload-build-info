b9976c71a1c90c8fb9e1549cc45981fb0440b32f
Nicola Paterson <nicolapaterson@Karen-Macbook-Pro.local>
2020-02-10 11:05:59 -0800

    add iTokenOptions

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { iBuild } from "./interfaces/iBuild";
+import { iTokenOptions } from "./interfaces/iTokenOptions";
 
 const core = require('@actions/core');
 const request = require('request-promise-native');
@@ -11,15 +12,15 @@ var tokenBodyData: any = {
     "client_secret": ''
 };
 
-var tokenOptions = {
-    method: 'POST',
-    url: 'https://api.atlassian.com/oauth/token',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: {}
-};
+// var tokenOptions = {
+//     method: 'POST',
+//     url: 'https://api.atlassian.com/oauth/token',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: {}
+// };
 
 let buildRef: any =
     {
@@ -122,8 +123,15 @@ async function getAccessToken() {
     tokenBodyData.client_id = clientId;
     tokenBodyData.client_secret = clientSecret;
     tokenBodyData = JSON.stringify(tokenBodyData);
-    tokenOptions.body = tokenBodyData;
-
+    const tokenOptions: iTokenOptions = {
+        method: 'POST',
+        url: 'https://api.atlassian.com/oauth/token',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: tokenBodyData,
+    };
     console.log("tokenOptions: ", tokenOptions);
     const response = await request(tokenOptions);
     console.log("getAccessToken response: ", response);
