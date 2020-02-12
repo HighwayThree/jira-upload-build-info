11e491ec5b350389b4b0317cb11721d48a944b6e
Nicola Paterson <nicolapaterson@Karen-Macbook-Pro.local>
2020-02-12 13:59:55 -0800

    try github.context.event.repositoryUri

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ async function submitBuildInfo(accessToken: any) {
     const buildRef: iBuildRef = {
         commit: {
             id: commitId || "",
-            repositoryUri: `${github.context.event.repository.url}/actions/runs/${github.context.run_id}` || buildRefUrl,
+            repositoryUri: `${github.context.event.repositoryUri}/actions/runs/${github.context.run_id}` || buildRefUrl,
         },
         ref: {
             name: "buildRef",
