3895ddcf94799b475f09ede0a6cf188c8070f611
Nicola Paterson <nicolapaterson@Karen-Macbook-Pro.local>
2020-02-12 13:57:49 -0800

    test github.context.event.repository.url

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ async function submitBuildInfo(accessToken: any) {
     const buildRef: iBuildRef = {
         commit: {
             id: commitId || "",
-            repositoryUri: buildRefUrl || `${github.context.event.repository.url}/actions/runs/${github.context.run_id}`,
+            repositoryUri: `${github.context.event.repository.url}/actions/runs/${github.context.run_id}` || buildRefUrl,
         },
         ref: {
             name: "buildRef",
