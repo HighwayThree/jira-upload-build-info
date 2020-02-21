cb722fa594c9806aba1e966cb7b379ea4ad2ef90
Nicola Paterson <nicolapaterson@Karen-Macbook-Pro.local>
2020-02-21 10:57:05 -0800

    refactor of npm module to not have to rely on github action specific modules

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const nock_1 = __importDefault(require("nock"));
 const index_1 = require("../index");
 require('jest-fetch-mock').enableMocks();
+const core = require('@actions/core');
 beforeEach(() => {
     jest.resetModules();
     fetchMock.resetMocks();
@@ -20,6 +21,42 @@ describe('debug action debug messages', () => {
     it('testing submitDeploymentInfo, no access token, expecting it to throw a reject', async () => {
         const fakeToken = '';
         await expect(index_1.submitBuildInfo(fakeToken)).rejects.toThrow();
+    });
+    it('is a test', async () => {
+        jest.spyOn(core, 'getInput').mockImplementation((name) => {
+            if (name === 'cloud-instance-base-url')
+                return 'https://example.atlassian.net/';
+            if (name === 'pipeline-id')
+                return 'test-jira-github-actions-demo CI';
+            if (name === 'build-number')
+                return '100';
+            if (name === 'update-sequence-number')
+                return '12345678';
+            if (name === 'build-display-name')
+                return 'Workflow: CI (#100)';
+            if (name === 'build-state')
+                return 'successful';
+            if (name === 'build-url')
+                return 'not-a-real-build';
+            if (name === 'last-updated')
+                return '2020-01-01T08:01:00Z';
+            if (name === 'issue-keys')
+                return '';
+            if (name === 'commit-id')
+                return '';
+            if (name === 'build-ref-url')
+                return '';
+            if (name === 'test-info-total-num')
+                return '';
+            if (name === 'test-info-num-passed')
+                return '';
+            if (name === 'test-info-num-failed')
+                return '';
+            if (name === 'test-info-num-skipped')
+                return '';
+            return '';
+        });
+        // await expect(submitBuildInfo('')).rejects;
     });
     // it('testing getAccessTokent, no spyOn', async () => {
     //   await expect(getAccessToken()).rejects.toThrow();
